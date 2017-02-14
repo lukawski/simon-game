4cffe8e00827ca743633ffe10ff299a9d4cb9858
Kacper Łukawski <ka.lukawski@gmail.com>
2017-02-14 23:00:18 +0100

    Game logic almost done

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 document.addEventListener('DOMContentLoaded', function (e) {
   const doc = document
   var btns = doc.querySelectorAll('.btn')
-  var steps = []
-  var nextStep = Math.floor(Math.random() * 4)
-  console.log(nextStep)
+  var steps = [Math.floor(Math.random() * 4)]
+  var userSteps = []
+
   btns.forEach(v => {
     v.addEventListener('click', function (e) {
-      if (Number(this.innerText) === nextStep) console.log('goode')
-      else console.log('wrong')
-      nextStep = Math.floor(Math.random() * 4 + 1)
-      console.log(nextStep)
+      userSteps.push(Number(this.innerText))
+
+      for (let i = 0; i < steps.length; i++) {
+        if (steps[i] !== userSteps[i]) {
+          console.log('bad')
+          userSteps = []
+          steps = []
+        }
+        console.log(steps, userSteps)
+      }
+
+      steps.push(Math.floor(Math.random() * 4 + 1))
     })
   })
 })

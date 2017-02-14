a840d716adcfa5f26e050bfbf9bf94a44d4983b7
Kacper Łukawski <ka.lukawski@gmail.com>
2017-02-14 22:37:07 +0100

    Work in progress

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-document.write('elo')
+document.addEventListener('DOMContentLoaded', function (e) {
+  const doc = document
+  var btns = doc.querySelectorAll('.btn')
+  var steps = []
+  var nextStep = Math.floor(Math.random() * 4)
+  console.log(nextStep)
+  btns.forEach(v => {
+    v.addEventListener('click', function (e) {
+      if (Number(this.innerText) === nextStep) console.log('goode')
+      else console.log('wrong')
+      nextStep = Math.floor(Math.random() * 4 + 1)
+      console.log(nextStep)
+    })
+  })
+})

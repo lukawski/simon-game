238d6a7443c00b35beed74d3b84cd17a2833e72d
Kacper Łukawski <ka.lukawski@gmail.com>
2017-02-15 23:59:12 +0100

    Meh

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ class Game {
 
     var _mode = 'normal'
     var _steps = [Math.floor(Math.random() * 4)]
-    var _userSteps = []
-    var btns = doc.querySelectorAll('.btn')
+    var _userStep = 4
+    var _btns = doc.querySelectorAll('.btn')
 
     // Public methods
     this.setMode = function (mode) {
@@ -13,7 +13,7 @@ class Game {
     }
 
     this.start = function () {
-      if (_mode === 'strict') 
+      if (_mode === 'strict')
         startStrictGame()
       else
         startNormalGame()
@@ -25,25 +25,27 @@ class Game {
 
     // Private methods
     function startNormalGame () {
-      btns.forEach(v => {
+      playSequence()
+      var stepsCounter = 0
+      _btns.forEach(v => {
         v.addEventListener('click', function (e) {
-          console.log(this.getAttribute('data-value'))
-          _userSteps.push(Number(this.getAttribute('data-value')))
-
-          for (let i = 0; i < _steps.length; i++) {
-            if (_steps[i] !== _userSteps[i]) {
-              _userSteps = []
-              _steps = []
-            }
+          _userStep = Number(this.getAttribute('data-value'))
+          console.log(_userStep, stepsCounter, _steps)
+          if (_userStep !== _steps[stepsCounter]) {
+            _userStep = 4
+            console.log('wrong')
+            playSequence()
+          } else {
+            _steps.push(Math.floor(Math.random() * 4))
+            playSequence()
+            stepsCounter++
           }
-
-          console.log(_steps)
         })
       })
     }
 
     function startStrictGame () {
-      btns.forEach(v => {
+      _btns.forEach(v => {
         v.addEventListener('click', function (e) {
           console.log(this.getAttribute('data-value'))
           _userSteps.push(Number(this.getAttribute('data-value')))
@@ -56,14 +58,31 @@ class Game {
           }
 
           _steps.push(Math.floor(Math.random() * 4))
-          btns[_steps[_steps.length - 1]].style.animationName = 'flash'
+          _btns[_steps[_steps.length - 1]].style.animationName = 'flash'
           console.log(_steps)
         })
       })
     }
 
     function playSequence () {
+      var i = 0
+      var interval = setInterval(() => {
+        if (i === _steps.length) {
+          clearInterval(interval)
+          removeAnimation()
+          return
+        }
+        _btns[_steps[i]].style.animationName = 'flash'
+        i++
+      }, 1000)
+    }
 
+    function removeAnimation () {
+      setTimeout(() => {
+        _btns.forEach(v => {
+          v.removeAttribute('style')
+        })
+      }, _steps.length * 500)
     }
   }
 }
